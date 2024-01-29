91c0afda640489fdaa5402e6f1113db614d497a7
Lundjo <lukadjelic529@gmail.com>
2024-01-29 21:36:38 +0100

    a

--- MVVM3/ViewModel/HomeViewModel.cs
+++ MVVM3/ViewModel/HomeViewModel.cs
@@ -11,7 +11,6 @@ namespace MVVM3.ViewModel
     public class HomeViewModel : BindableBase
     {
         private string applicationTitle;
-        private string applicationSubtitle;
 
         public MyICommand ImportXml { get; set; }
         public MyICommand ResetAll { get; set; }
@@ -19,7 +18,6 @@ namespace MVVM3.ViewModel
         public HomeViewModel()
         {
             ApplicationTitle = "GDA Client";
-            ApplicationSubtitle = "Use navigation to access advanced options";
 
             ImportXml = new MyICommand(ImportXmlData);
             ResetAll = new MyICommand(ClearData);
@@ -40,16 +38,14 @@ namespace MVVM3.ViewModel
                 if (File.Exists(dataPath))
                 {
                     File.Delete(dataPath);
-                    Messenger.Default.Send(new StatusMessage("NetworkModelData.data file deleted successfully.", "SteelBlue"));
                 }
                 else
                 {
-                    Messenger.Default.Send(new StatusMessage("NetworkModelData.data file does not exist.", "Firebrick"));
                 }
             }
             catch (Exception ex)
             {
-                Messenger.Default.Send(new StatusMessage($"An error occurred: {ex.Message}", "Firebrick"));
+                
             }
         }
 
@@ -60,16 +56,6 @@ namespace MVVM3.ViewModel
             {
                 applicationTitle = value;
                 OnPropertyChanged("ApplicationTitle");
-            }
-        }
-
-        public string ApplicationSubtitle
-        {
-            get { return applicationSubtitle; }
-            set
-            {
-                applicationSubtitle = value;
-                OnPropertyChanged("ApplicationSubtitle");
             }
         }
     }
